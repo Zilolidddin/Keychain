9b78864845dddfa27ba627ca174055a7ec2cbe9c
Ziloliddin <Zilolidddinkamolov@gmail.com>
2023-01-22 11:14:24 +0500

    Initial Commit

---
+++ Keychain/KeychainApp.swift
@@ -0,0 +1,17 @@
+//
+//  KeychainApp.swift
+//  Keychain
+//
+//  Created by Ziloliddin Kamolov on 22/01/23.
+//
+
+import SwiftUI
+
+@main
+struct KeychainApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
